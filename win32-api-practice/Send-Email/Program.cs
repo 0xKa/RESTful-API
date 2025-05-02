bf801eb522c33257c691ac4e94e8e2ec55e49782
Reda Hilal <reda0x04@gmail.com>
2025-05-03 00:38:00 +0400

    send email

---
+++ win32-api-practice/Send-Email/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Outlook.Application outlookApp = new Outlook.Application();
+            Outlook.MailItem mailItem = (Outlook.MailItem)outlookApp.CreateItem(Outlook.OlItemType.olMailItem);
+            mailItem.Subject = "Test Email from C#";
+            mailItem.To = "reda0x04@gmail.com";
+            mailItem.Body = "Yo, this is a test email sent from a C# application using Outlook Interop.";
+            mailItem.Importance = Outlook.OlImportance.olImportanceHigh;
+
+            // Don't display if you want to send automatically
+            mailItem.Send();
+
+            Console.WriteLine("Email sent successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
