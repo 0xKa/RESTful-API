bc71cd8cd88fe736cc15c1252d91814b08d4e3cd
Reda Hilal <reda0x04@gmail.com>
2025-05-05 03:57:12 +0400

    edit

--- Web-API-Project/Controllers/MyFirstAPIController.cs
+++ Web-API-Project/Controllers/MyFirstAPIController.cs
@@ -21,17 +21,30 @@ namespace Web_API_Project.Controllers
             return "Sample Text...";
         }
 
-        [HttpGet("Sum/{num1},{num2}")]
+        [HttpGet("Sum/{num1}/{num2}")]
         public double Sum2Num(double num1, double num2)
         {
             return num1 + num2;
         }
         
-        [HttpGet("Multi/{num1},{num2}")]
+        [HttpGet("Multi/{num1}/{num2}")]
         public double Multi2Num(double num1, double num2)
         {
             return num1 * num2;
         }
+
+        [HttpGet("Divide/{a}/{b}")]
+        //note: we can use ActionResult<double> instead of double to return Either a double or a custom status code + message
+        public ActionResult<double> Divide(double a, double b)
+        {
+            if (b == 0)
+                return BadRequest("Cannot divide by zero"); // returns 400 Bad Request
+
+            return Ok(a / b); // returns 200 OK with result
+        }
+
+
+
 
     }
 }
