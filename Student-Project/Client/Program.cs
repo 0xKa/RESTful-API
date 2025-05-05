c3a3a6980613abf598d859fb95003215f9fde984
Reda Hilal <reda0x04@gmail.com>
2025-05-05 23:09:07 +0400

    restructure project and implement client side

---
+++ Student-Project/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using Student_API_Project.Model;
+using System.Net.Http.Json;
+
+class Programm
+{
+    static readonly HttpClient httpClient = new HttpClient();
+
+    public static async Task Main()
+    {
+        httpClient.BaseAddress = new Uri("http://localhost:5210/api/Students/");
+
+        await GetAllStudents();
+
+    }
+
+    static async Task GetAllStudents()
+    {
+        try
+        {
+            Console.WriteLine("\nFetching All Students...");
+            var students = await httpClient.GetFromJsonAsync<List<Student>>("AllStudents");
+
+            if (students != null)
+            {
+                foreach (var student in students)
+                {
+                    student.PrintCard();
+                }
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error Message: " + ex.Message);
+        }
+
+
+    }
+}
