beeb704ad4495a3b7a443ea179115fc529de79b2
Reda Hilal <reda0x04@gmail.com>
2025-05-05 21:59:13 +0400

    server side student project

---
+++ Student-API-Project/Controllers/StudentAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Student_API_Project.Model;
+using Student_API_Project.DatabaseSimulation;
+
+namespace Student_API_Project.Controllers
+{
+    [Route("api/Students")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+
+        [HttpGet("AllStudents")]
+        public ActionResult<IEnumerable<Student>> GetALlStudents()
+        {
+            return Ok(Database.Students);
+        }
+
+
+
+    }
+}
